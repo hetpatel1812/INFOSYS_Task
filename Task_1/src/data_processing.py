240222640ae7b5f84ca24f5782ab15a45b85807c
Het Patel <hetpce2005@gmail.com>
2025-10-13 15:08:55 +0530

    Delete Task_1 directory

--- Task_1/src/data_processing.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-def load_data(file):
-    if file.name.endswith('.csv'):
-        return pd.read_csv(file)
-    elif file.name.endswith('.json'):
-        return pd.read_json(file)
-    else:
-        raise ValueError("Unsupported file format. Please upload CSV or JSON.")
--- Task_1/src/utils.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-def dataset_info(df: pd.DataFrame):
-    return {
-        "Rows": df.shape[0],
-        "Columns": df.shape[1],
-        "Column Names": list(df.columns)
-    }
